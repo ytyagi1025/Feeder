2c5d2a27d322900f7562fb827ef8afda9ec20a2a
Yash Tyagi <ytyagi@opsecsecurity.com>
2025-01-08 21:16:22 +0530

    first commit

---
+++ src/main/java/com/lucas/feeder/FeederApplication.java
@@ -0,0 +1,13 @@
+package com.lucas.feeder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeederApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FeederApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lucas/feeder/FeederApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucas.feeder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeederApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
